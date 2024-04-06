66efa5c95502860da7be0eff9f7ed02885be19ed
rpm <rusenmanev@gmail.com>
2024-04-06 23:13:45 +0300

    test 2

---
+++ 3_Python_Advanced/2_exercises/7_wotkshop_tik_tak_toe/main.py
@@ -0,0 +1,19 @@
+continents = {
+  "Europe": {
+    "Bulgaria": 7000000,
+    "Serbia": 10000000
+  },
+  "Asia": {
+    "Mongolia": 19000000,
+    "Iran": 50000000,
+    "Kazakstan": 73000000,
+    "Japan": 150000000
+  },
+  "South America": {
+    "Brazil": 50000000,
+    "Peru": 45000000,
+    "Bolivia": 15000000,
+    "Argentina": 49000000
+  }
+}
+
